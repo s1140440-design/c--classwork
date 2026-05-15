af6e96d22c7d468af31fbc8c47c5c52fe5f5bb14
s1140440-design <s1140440@dmhs.kh.edu.tw>
2026-05-15 11:24:34 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int age;
+	string name;
+	cout<<"how old are you?"<<endl;
+	cin>> age;
+	cout<<"what your name?"<<endl;
+	cin>>name;
+	cout<<"你是"<<name<<endl;
+	cout<<"今年"<<age<<"歲";
+}
